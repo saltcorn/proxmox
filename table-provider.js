47d8e028832fce258ea56b95ec1dc49248e1d0d8
Tom Nielsen <tanielsen@gmail.com>
2025-04-30 17:18:14 +0100

    node table provider

--- table-provider.js
+++ table-provider.js
@@ -33,25 +33,48 @@ const configuration_workflow = (cfg) => (req) =>
 
 const runQuery = async (cfg, where, opts) => {
   console.log(proxmoxApi);
-  
+
   const proxmox = proxmoxApi({
     host: cfg.host,
     password: cfg.password,
     username: cfg.username,
   });
   // list nodes
-  const nodes = await proxmox.nodes.$get();
-  console.log(nodes);
+  switch (cfg.entity_type) {
+    case "Nodes":
+      const nodes = await proxmox.nodes.$get();
+      return nodes;
 
-  return [];
+    default:
+      break;
+  }
 };
 
 module.exports = (modcfg) => ({
   Proxmox: {
     configuration_workflow: configuration_workflow(modcfg),
-    fields: (cfgTable) => [
-      { name: "url", type: "String", label: "URL", primary_key: true },
-    ],
+    fields: (cfgTable) => {
+      switch (cfgTable.entity_type) {
+        case "Nodes":
+          return [
+            { name: "id", type: "String", label: "ID", primary_key: true },
+            { name: "status", type: "String", label: "Status" },
+            { name: "node", type: "String", label: "Node" },
+            { name: "level", type: "String", label: "Level" },
+            { name: "ssl_fingerprint", type: "String", label: "SSL fingerprint" },
+            { name: "disk", type: "Integer", label: "Disk" },
+            { name: "maxdisk", type: "Integer", label: "Maximum Disk" },
+            { name: "mem", type: "Integer", label: "Memory" },
+            { name: "maxmem", type: "Integer", label: "Maximum Memory" },
+            { name: "cpu", type: "Float", label: "CPU" },
+            { name: "maxcpu", type: "Integer", label: "Maximum CPU" },
+            { name: "uptime", type: "Integer", label: "Uptime" },
+          ];
+
+        default:
+          return []
+      }
+    },
     get_table: (cfgTable) => {
       return {
         getRows: async (where, opts) => {
